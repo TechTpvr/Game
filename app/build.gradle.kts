3f78e74ea307970c632f65b497439d0921343e50
TechTpvr <farrokhzadpvr@gmail.com>
2026-09-17 21:51:54 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,14 +2,25 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.pocketarcade.app"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.pocketarcade.app"
         minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
